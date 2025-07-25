ee958fb4ab319ba1c362bedfb84d51b32f472232
sangeetashirsat <66305829+sangeetashirsat@users.noreply.github.com>
2025-07-25 10:51:31 +0530

    Updated build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1bb77fd76012454d"; }
+/******/ 		__webpack_require__.h = function() { return "6b34d9886d29ffa7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
