e921ee619de614fe499e0d6f0441b7d466cb2cad
sangeetashirsat <66305829+sangeetashirsat@users.noreply.github.com>
2025-07-25 10:50:44 +0530

    Add missing Supabase environment variables

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c38e5e6befa0bc3"; }
+/******/ 		__webpack_require__.h = function() { return "1bb77fd76012454d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
